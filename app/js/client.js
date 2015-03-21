9e49cf2d0c64a5650f4ec185113f184d4890fa64
StephanieBeaton <stefanybeaton@gmail.com>
2015-03-20 17:39:05 -0700

    display form to create user

--- app/js/client.js
+++ app/js/client.js
@@ -1,21 +1,83 @@
 'use strict';
 
-require('craftyjs');
+// require('craftyjs');
 
-// create container node for canvas/game
-var pNode = document.createElement('div');
-pNode.setAttribute('id', 'game')
+// // Crafty Specifics
+// Crafty.init(500, 500, document.getElementById('game'));
 
-// identify our build definition so we can insert the canvas/game before it
-var script = document.getElementsByTagName('script')[0];
-document.getElementsByTagName('body')[0].insertBefore(pNode, script);
+// Crafty.background('#000000');
 
-// Crafty Specifics
-Crafty.init(500, 500, document.getElementById('game'));
+// Crafty.e('2D, Canvas, Color, Fourway')
+// 	.attr({x: 10, y: 10, w: 20, h: 20})
+// 	.color('#00ff00')
+// 	.fourway(4);
 
-Crafty.background('#000000');
+var React = require('react');
+var Fluxxor = require('fluxxor');
+var request = require('superagent');
+var constants = require('./constants');
 
-Crafty.e('2D, Canvas, Color, Fourway')
-	.attr({x: 10, y: 10, w: 20, h: 20})
-	.color('#00ff00')
-	.fourway(4);
+//users and session
+var UserStore = require('./users/user_store');
+var CreateUser = require('./users/components/create_user');
+
+var actions = {
+  login: function(user) {
+    this.dispatch(constants.LOGIN, user);
+  },
+
+  logout: function() {
+    this.dispatch(constants.LOGOUT);
+  },
+
+  createUser: function(user) {
+    this.dispatch(constants.CREATE_USER, user);
+  },
+
+  displayCreateUser: function() {
+    this.dispatch(constants.DISPLAY_CREATE_USER);
+  }
+};
+
+var stores = {
+  UserStore: new UserStore()
+};
+
+var flux = new Fluxxor.Flux(stores, actions);
+
+var FluxMixin = Fluxxor.FluxMixin(React);
+var StoreWatchMixin = Fluxxor.StoreWatchMixin;
+
+var  createUserClicked = false;
+
+var App = React.createClass({
+  mixins: [FluxMixin, StoreWatchMixin('UserStore')],
+
+
+  getStateFromFlux: function() {
+    var flux = this.getFlux();
+    return {
+      userData: flux.store('UserStore').getState()
+    };
+  },
+  handleCreateUserButton: function(e) {
+    e.preventDefault();
+    this.getFlux().actions.displayCreateUser();
+    createUserClicked = true;
+  },
+
+  render: function() {
+    //var createUser = <a href onClick={this.handleCreateUserButton}>Create User</a>;
+    var createUser = <button onClick={this.handleCreateUserButton}>Create User</button>;
+    var homePageCenterPanel = createUser;
+    if (createUserClicked) homePageCenterPanel = <CreateUser />
+    return (
+      <main>
+        {homePageCenterPanel}
+      </main>
+    )
+  }
+});
+
+React.render(<App flux={flux}/>, document.body);
+
