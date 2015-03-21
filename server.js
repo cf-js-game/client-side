0fe0590bc5acc319b986a21b417ef036eed789a6
Rory Sterley <royrsterley@gmail.com>
2015-03-20 18:57:41 -0700

    Add user routes and tests

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 
 var express = require('express');
 var mongoose = require('mongoose');
+var passport = require('passport');
+var passport_strat = require('./lib/passport_strat');
+var userRoutes = require('./routes/user_routes');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -10,6 +13,19 @@ app.use(express.static(__dirname + '/build'));
 
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/jsgame_dev');
 
+// Security init
+app.set('appSecret', process.env.SECRET || 'loot!loot!loot!loot!');
+app.use(passport.initialize());
+passport_strat(passport);
+
+// Routes
+var userRouter = express.Router();
+
+userRoutes(userRouter, passport, app.get('appSecret'));
+
+app.use('/api/v1', userRouter);
+
+// Server init
 app.listen(port, function() {
-  console.log('server listening on port ' + port);
+  console.log('Server listening on port ' + port + '.');
 });
---
+++ test/server/users-api-test.js
@@ -0,0 +1,65 @@
+'use strict';
+
+process.env.MONGO_URI = 'mongodb://localhost/jsgame_test';
+require('../../server.js');
+var chai = require('chai');
+var chaihttp = require('chai-http');
+var mongoose = require('mongoose');
+var User = require('../../models/User');
+
+chai.use(chaihttp);
+
+var expect = chai.expect;
+
+var testEmail = 'test@example.com';
+var testPassword = 'your cats name';
+var routesBase = 'localhost:3000/api/v1';
+
+describe('user endpoints', function () {
+  var id;
+  var tempToken;
+
+  before(function (done) {
+    var newUser = new User();
+    newUser.basic.email = testEmail;
+    newUser.basic.password = newUser.generateHash(testPassword);
+    newUser.save(function (err, user) {
+      if (err) return console.error('could not create user');
+      chai.request(routesBase)
+        .get('/sign_in')
+        .auth(testEmail, testPassword)
+        .end(function (err, res) {
+          tempToken = res.body.token;
+          done();
+        });
+    });
+  });
+
+  after(function (done) {
+    mongoose.connection.db.dropDatabase(function () {
+      done();
+    });
+  });
+
+  it('should create a user', function (done) {
+    chai.request(routesBase)
+      .post('/create_user')
+      .send({"basic.email": "t2@example.com", "basic.password": testPassword})
+      .end(function (err, res) {
+        expect(err).to.eql(null);
+        expect(res.body).to.have.property('token');
+        done();
+      });
+  });
+
+  it('should sign-in', function (done) {
+      chai.request(routesBase)
+        .get('/sign_in')
+        .auth(testEmail, testPassword)
+        .end(function (err, res) {
+          expect(err).to.eql(null);
+          expect(res.body).to.have.property('token');
+          done();
+        });
+    });
+});
