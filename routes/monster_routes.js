84cc1040997945c6ac36b3d21dd8eb048195f1d4
Rory Sterley <royrsterley@gmail.com>
2015-03-25 16:31:09 -0700

    Coment out monster_routes.js.Lint people lint

--- routes/monster_routes.js
+++ routes/monster_routes.js
@@ -1,14 +1,19 @@
-'use strict';
-var bodyparser = require('body-parser');
-var Monster = require('../models/Monster');
-var MonsterQ = require('../models/MonsterQ');
+// 'use strict';
+// var bodyparser = require('body-parser');
+// var Monster = require('../models/Monster');
+// var MonsterQ = require('../models/MonsterQ');
 
-module.exports = function(monster) {
-  monster.use(bodyparser.json());
+/**
+ * Fix this before uncommenting
+ * Daniel, lint your code!
+ */
 
-  monster.get('/monster_list', function(req, res) {
-      if (err) return res.status(500).send({'msg': 'could not retrieve monster(s)'});
-      res.json(data);
-    });
-  });
-};
+// module.exports = function(monster) {
+//   monster.use(bodyparser.json());
+
+//   monster.get('/monster_list', function(req, res) {
+//       if (err) return res.status(500).send({'msg': 'could not retrieve monster(s)'});
+//       res.json(data);
+//     });
+//   });
+// };
